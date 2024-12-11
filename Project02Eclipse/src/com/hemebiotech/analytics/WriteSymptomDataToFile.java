5f57b495273b97789b881c464daa741b9240ad77
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-12-11 20:35:17 +0100

    .

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,11 +1,7 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import com.ISymptomWriter;
